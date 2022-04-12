693e38caf0b2498e66681b51ea04c9ac3c59f1b3
Руслан Яндиев <jandiev@bk.ru>
2022-04-12 19:06:17 +0300

    intermediate version, will continue later

--- homework.py
+++ homework.py
@@ -35,23 +35,21 @@ class Training:
 
     def get_distance(self) -> float:
         """Получить дистанцию в км."""
-
-        return self.action * Training.LEN_STEP / Training.M_IN_KM
+        return self.action * self.LEN_STEP / Training.M_IN_KM
 
     def get_mean_speed(self) -> float:
         """Получить среднюю скорость движения."""
-
         return self.get_distance() / self.duration
 
     def get_spent_calories(self) -> float:
         """Получить количество затраченных калорий."""
-    # "Вместо pass тут отлично подойдет исключение NotImplementedError."
-    # Это должно выглядеть так?
-        return NotImplementedError
+        raise NotImplementedError('Метод не должен вызываться '
+                                  'в классе Training')
+    # Я так понял, что можно что угодно тут писать,
+    # лишь бы коллеги поняли, верно?
 
     def show_training_info(self) -> InfoMessage:
         """Вернуть информационное сообщение о выполненной тренировке."""
-
         return InfoMessage(type(self).__name__, self.duration,
                            self.get_distance(), self.get_mean_speed(),
                            self.get_spent_calories())
@@ -60,22 +58,20 @@ class Training:
 class Running(Training):
     """Тренировка: бег."""
 
-    CAL_RATE_1 = 18
-    CAL_RATE_2 = 20
+    AVG_SPEED_COEFF_1 = 18
+    AVG_SPEED_COEFF_2 = 20
 
     def get_spent_calories(self) -> float:
-        return ((self.CAL_RATE_1 * self.get_mean_speed()
-                - self.CAL_RATE_2) * self.weight / Training.M_IN_KM
+        return ((self.AVG_SPEED_COEFF_1 * self.get_mean_speed()
+                - self.AVG_SPEED_COEFF_2) * self.weight / Training.M_IN_KM
                 * self.duration * self.IN_MINUTES)
 
 
 class SportsWalking(Training):
     """Тренировка: спортивная ходьба."""
 
-    LEN_STEP: float = 0.68
-    M_IN_KM: int = 1000
-    CAL_RATE_3 = 0.035
-    CAL_RATE_4 = 0.029
+    WEIGHT_COEFF_1 = 0.035  # weight_coeff тк. влияет на вес
+    AVG_SPEED_COEFF_3 = 0.029  # avg_speed_coeff тк. влияет на среднюю скорость
     SPEED_MULTIPLIER = 2
 
     def __init__(self, action, duration, weight, height):
@@ -83,51 +79,48 @@ class SportsWalking(Training):
         self.height = height
 
     def get_spent_calories(self):
-        return ((self.CAL_RATE_3 * self.weight
+        return (self.WEIGHT_COEFF_1 * self.weight
                 + (self.get_mean_speed() ** self.SPEED_MULTIPLIER
-                 // self.height)
-                 * self.CAL_RATE_4) * (self.duration * self.IN_MINUTES))
+                   // self.height)
+                * self.AVG_SPEED_COEFF_3) * (self.duration * self.IN_MINUTES)
 
 
 class Swimming(Training):
     """Тренировка: плавание."""
 
     LEN_STEP: float = 1.38
-    CAL_RATE_5 = 1.1
-    CAL_RATE_6 = 2
+    WEIGHT_COEFF_2 = 2
+    AVG_SPEED_COEFF_4 = 1.1
 
     def __init__(self, action, duration, weight, length_pool, count_pool):
         super().__init__(action, duration, weight)
         self.length_pool = length_pool
         self.count_pool = count_pool
-
-    def get_distance(self):
-        return (self.action * Swimming.LEN_STEP) / Training.M_IN_KM
+    # Тут был переопределяемый get_distance
+    # долго не получалось заставить всё работать без переопределения
+    # в итоге додумался изменить метод в родительском классе:
+    # Training.LEN_STEP ---> self.LEN_STEP
 
     def get_mean_speed(self) -> float:
         return (self.length_pool * self.count_pool
                 / Training.M_IN_KM / self.duration)
 
     def get_spent_calories(self):
-        return ((self.get_mean_speed() + self.CAL_RATE_5)
-                * self.CAL_RATE_6 * self.weight)
+        return ((self.get_mean_speed() + self.AVG_SPEED_COEFF_4)
+                * self.WEIGHT_COEFF_2 * self.weight)
 
 
 def read_package(workout_type: str, data: list):
     """Прочитать данные полученные от датчиков."""
 
-    dicti: Dict[str, Type[Training]] = {
+    workout_types: Dict[str, Type[Training]] = {
         'RUN': Running,
         'WLK': SportsWalking,
         'SWM': Swimming,
     }
-    if workout_type not in dicti:
-        # "...лучше вообще развернуть проверку наоборот..."
-        # "...аварийный возврат с исключением и текстом ошибки с ключом..."
-        raise NotImplementedError("Неожиданный тип тренировки")
-        # Так? Или по-другому?
-    else:
-        return dicti[workout_type](*data)
+    if workout_type not in workout_types:
+        raise ValueError("Неожиданный тип тренировки: workout_type")
+    return workout_types[workout_type](*data)
 
 
 def main(training: Training) -> None:
@@ -145,10 +138,8 @@ if __name__ == '__main__':
     ]
 
     for workout_type, data in packages:
-        # "Ловить нужно исключение"
-        if read_package(workout_type, data) is None:
-            # Имелось ввиду, что нужно местами поменять if и else
+        try:
             main(read_package(workout_type, data))
-            # Или нужно было использовать try/except?
-        else:
-            print('Неожиданный тип тренировки')
+        except KeyError as key_e:
+            print(f'Ошибка. Отсуствует ключ {key_e} '
+                  'в словаре workout_types')
